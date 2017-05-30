b0c8698faefd74532a19dee0d047467240065d1b
George Paley <George@JDPaley.com>
2017-05-30 09:27:28 -0400

    Add CopyFizzBuzzCat

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -2,12 +2,14 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		String msg;
 		for (int i=1; i<101; i++) {
 			msg="";
+			if (i%2 == 0) {msg += "Copy";}
 			if (i%3 == 0) {msg += "Fizz";}
 			if (i%5 == 0) {msg += "Buzz";}
+			if (i%10 == 0) {msg += "Cat";}
 			if (msg.equals("")) {msg= "" + i;}
 			System.out.println(msg);
 			}
